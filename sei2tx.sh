19072efdc53193b19cdcfaa30bf49156fad38fdc
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-12-03 06:54:54 +0700

    Update sei2tx.sh

--- sei2tx.sh
+++ sei2tx.sh
@@ -7,12 +7,12 @@ echo '{
     "messages": [
       {
         "@type": "/seiprotocol.seichain.dex.MsgPlaceOrders",
-        "creator": "${seiwallet}",
+        "creator": "SEIWALLET",
         "orders": [
           {
             "id": "0",
             "status": "PLACED",
-            "account": "${seiwallet}",
+            "account": "SEIWALLET",
             "contractAddr": "sei1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqpeheyc",
             "price": "1.000000000000000000",
             "quantity": "0.000010000000000000",
@@ -34,12 +34,12 @@ echo '{
       },
       {
         "@type": "/seiprotocol.seichain.dex.MsgPlaceOrders",
-        "creator": "${seiwallet}",
+        "creator": "SEIWALLET",
         "orders": [
           {
             "id": "0",
             "status": "PLACED",
-            "account": "${seiwallet}",
+            "account": "SEIWALLET",
             "contractAddr": "sei1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqpeheyc",
             "price": "1.000000000000000000",
             "quantity": "0.000010000000000000",
@@ -84,23 +84,27 @@ echo '{
 }' > $HOME/seiex/gen_2tx.json
 
 
+NSEIWALLET=$seiwallet
+
+sed -i s/SEIWALLET/${NSEIWALLET}/ $HOME/seiex/gen_2tx.json
 
 sleep 3
 
-ACC=$(seid query account ${seiwallet} -o json | jq -r .account_number)
+ACC=$(seid query account $seiwallet -o json | jq -r .account_number)
+ACC=$(seid q account $seiwallet -o json --node https://sei-testnet-rpc.brocha.in:443 | jq -r .account_number)
 
 sleep 3
 
 
-seq=$(seid query account ${seiwallet} -o json | jq -r .sequence)
+seq=$(seid query account $seiwallet  -o json | jq -r .sequence)
+seq=$(seid q account $seiwallet -o json --node https://sei-testnet-rpc.brocha.in:443 | jq -r .sequence)
 
 sleep 3
 
 seid tx sign /root/seiex/gen_2tx.json -s $seq -a $ACC --offline \
---from ${seiwallet} --chain-id atlantic-1 \
+--from $seiwallet --chain-id atlantic-1 \
 --output-document /root/seiex/txs.json
 
 sleep 3
 
 seid tx broadcast /root/seiex/txs.json
-
