8f4410fdf3ef6593aa4faf5eede567882825b68f
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-12-03 07:35:56 +0700

    Update sei2tx.sh

--- sei2tx.sh
+++ sei2tx.sh
@@ -2,6 +2,9 @@
 
 read -p "Enter sei wallet address: " seiwallet
 
+
+
+function LongLong {
 echo '{
   "body": {
     "messages": [
@@ -81,30 +84,257 @@ echo '{
   },
   "signatures": []
 }' > $HOME/seiex/gen_2tx.json
+}
 
 
+
+function LongShort {
+echo '{
+  "body": {
+    "messages": [
+      {
+        "@type": "/seiprotocol.seichain.dex.MsgPlaceOrders",
+        "creator": "SEIWALLET",
+        "orders": [
+          {
+            "id": "0",
+            "status": "PLACED",
+            "account": "SEIWALLET",
+            "contractAddr": "sei1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqpeheyc",
+            "price": "1.000000000000000000",
+            "quantity": "0.000010000000000000",
+            "priceDenom": "UST2",
+            "assetDenom": "ATOM",
+            "orderType": "LIMIT",
+            "positionDirection": "LONG",
+            "data": "{\"position_effect\":\"Open\",\"leverage\":\"1\"}",
+            "statusDescription": ""
+          }
+        ],
+        "contractAddr": "sei1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqpeheyc",
+        "funds": [
+          {
+            "denom": "uusdc",
+            "amount": "10"
+          }
+        ]
+      },
+      {
+        "@type": "/seiprotocol.seichain.dex.MsgPlaceOrders",
+        "creator": "SEIWALLET",
+        "orders": [
+          {
+            "id": "0",
+            "status": "PLACED",
+            "account": "SEIWALLET",
+            "contractAddr": "sei1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqpeheyc",
+            "price": "1.000000000000000000",
+            "quantity": "0.000010000000000000",
+            "priceDenom": "UST2",
+            "assetDenom": "ATOM",
+            "orderType": "LIMIT",
+            "positionDirection": "SHORT",
+            "data": "{\"position_effect\":\"Open\",\"leverage\":\"1\"}",
+            "statusDescription": ""
+          }
+        ],
+        "contractAddr": "sei1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqpeheyc",
+        "funds": [
+          {
+            "denom": "uusdc",
+            "amount": "10"
+          }
+        ]
+      }
+    ],
+    "memo": "",
+    "timeout_height": "0",
+    "extension_options": [],
+    "non_critical_extension_options": []
+  },
+  "auth_info": {
+    "signer_infos": [],
+    "fee": {
+      "amount": [
+        {
+          "denom": "usei",
+          "amount": "0"
+        }
+      ],
+      "gas_limit": "300000",
+      "payer": "",
+      "granter": ""
+    }
+  },
+  "signatures": []
+}' > $HOME/seiex/gen_2tx.json
+}
+
+
+function SHORTSHORT {
+echo '{
+  "body": {
+    "messages": [
+      {
+        "@type": "/seiprotocol.seichain.dex.MsgPlaceOrders",
+        "creator": "SEIWALLET",
+        "orders": [
+          {
+            "id": "0",
+            "status": "PLACED",
+            "account": "SEIWALLET",
+            "contractAddr": "sei1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqpeheyc",
+            "price": "1.000000000000000000",
+            "quantity": "0.000010000000000000",
+            "priceDenom": "UST2",
+            "assetDenom": "ATOM",
+            "orderType": "LIMIT",
+            "positionDirection": "SHORT",
+            "data": "{\"position_effect\":\"Open\",\"leverage\":\"1\"}",
+            "statusDescription": ""
+          }
+        ],
+        "contractAddr": "sei1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqpeheyc",
+        "funds": [
+          {
+            "denom": "uusdc",
+            "amount": "10"
+          }
+        ]
+      },
+      {
+        "@type": "/seiprotocol.seichain.dex.MsgPlaceOrders",
+        "creator": "SEIWALLET",
+        "orders": [
+          {
+            "id": "0",
+            "status": "PLACED",
+            "account": "SEIWALLET",
+            "contractAddr": "sei1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqpeheyc",
+            "price": "1.000000000000000000",
+            "quantity": "0.000010000000000000",
+            "priceDenom": "UST2",
+            "assetDenom": "ATOM",
+            "orderType": "LIMIT",
+            "positionDirection": "SHORT",
+            "data": "{\"position_effect\":\"Open\",\"leverage\":\"1\"}",
+            "statusDescription": ""
+          }
+        ],
+        "contractAddr": "sei1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqpeheyc",
+        "funds": [
+          {
+            "denom": "uusdc",
+            "amount": "10"
+          }
+        ]
+      }
+    ],
+    "memo": "",
+    "timeout_height": "0",
+    "extension_options": [],
+    "non_critical_extension_options": []
+  },
+  "auth_info": {
+    "signer_infos": [],
+    "fee": {
+      "amount": [
+        {
+          "denom": "usei",
+          "amount": "0"
+        }
+      ],
+      "gas_limit": "300000",
+      "payer": "",
+      "granter": ""
+    }
+  },
+  "signatures": []
+}' > $HOME/seiex/gen_2tx.json
+}
+
+
+
+function Createwallet {
 NSEIWALLET=$seiwallet
-
 sed -i s/SEIWALLET/${NSEIWALLET}/ $HOME/seiex/gen_2tx.json
-
-sleep 3
-
 ACC=$(seid query account $seiwallet -o json | jq -r .account_number)
-
-
-sleep 3
-
-
 seq=$(seid query account $seiwallet  -o json | jq -r .sequence)
+}
 
 
-sleep 3
-
+function CreatTX {
 seid tx sign /root/seiex/gen_2tx.json -s $seq -a $ACC --offline \
 --from $seiwallet --chain-id atlantic-1 \
 --output-document /root/seiex/txs.json
+}
 
 
-sleep 3
+function CreatTXNOT {
+read -p "Enter sei wallet address: " sequenceid
+seid tx sign /root/seiex/gen_2tx.json -s $seq -a $ACC --offline \
+--from $seiwallet --chain-id atlantic-1 \
+--sequence ${sequenceid} \
+--output-document /root/seiex/txs.json
+}
 
+
+
+function Broadcast {
 seid tx broadcast /root/seiex/txs.json
+}
+
+
+
+PS3='Please enter your choice (input your option number and press enter): '
+options=("LONGLONG" "LONGSHORT" "SHORTSHORT" "Fix error sequence" "Quit")
+
+
+select opt in "${options[@]}"
+do
+    case $opt in
+        "LONGLONG")
+            echo -e '\e[1m\e[32mYou choose Place multiple orders LONG LONG...\e[0m' && sleep 1
+LongLong
+Createwallet
+CreatTX
+Broadcast
+break
+;;
+
+"LONGSHORT")
+            echo -e '\e[1m\e[32mYou choose Place multiple orders LONG SHORT...\e[0m' && sleep 1
+LongShort
+Createwallet
+CreatTX
+Broadcast
+break
+;;
+
+
+"SHORTSHORT")
+            echo -e '\e[1m\e[32mYou choose Place multiple orders SHORT SHORT...\e[0m' && sleep 1
+SHORTSHORT
+Createwallet
+CreatTX
+Broadcast
+break
+;;
+
+
+"Fix error sequence")
+            echo -e '\e[1m\e[32mYou choose Fix error sequence...\e[0m' && sleep 1
+CreatTX
+Broadcast
+break
+;;
+
+
+
+"Quit")
+break
+;;
+
+*) echo -e "\e[91minvalid option $REPLY\e[0m";;
+    esac
+done
